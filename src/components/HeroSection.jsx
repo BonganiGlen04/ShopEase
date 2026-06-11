8e4599627be9ffc17d1a0c58b7ce680ba82489ad
Bongani Maduma <135016371+BonganiGlen04@users.noreply.github.com>
2026-06-11 16:26:57 +0200

    Add Navbar and HeroSection components; include assets and constants

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,25 @@
+import heroImage from "../assets/hero-image.png";
+
+const HeroSection = () => {
+    return(
+        <div className="flex flex-wrap items-center justify-center gap-8 w-[82%] m-auto mt-28 lg:flex-nowrap lg:gap-0 lg:justify-between">
+            <div className="flex flex-col gap-4 w-full">
+                <span className="text-[3.25rem] font-medium">Shop Smarter. Shop Better.</span>
+                <span className="w-full lg:w-152">
+                    Discover thousands of products, each carefully selected for quality,
+                    style, and value bringing everything you need and love into one 
+                    convenient place.
+                </span>
+                <div className="flex gap-4 mt-8">
+                    <button className="bg-black text-white px-8 py-3 rounded-medium font-medium cursor-pointer hover:bg-black/90">Show Now</button>
+                    <button className="bg-black text-white px-8 py-3 rounded-medium font-medium cursor-pointer hover:bg-black/90">Explore Deals</button>
+                </div>
+            </div>
+            <img 
+            src={heroImage} 
+            alt="Hero image" 
+            className="w-[20rem] lg:w-120 lg:h-100" />
+        </div>
+    )
+}
+export default HeroSection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,26 @@
-function Navbar() {
-  return <div>Navbar</div>
-}
+import cartIcon from "../assets/cart-icon.svg";
+
+const Navbar = () => {
+  return (
+    <div className="bg-black text-white flex justify-between py-4 px-16">
+      <div className="flex items-center justify-center gap-5">
+        <span className="text-lg font-medium cursor-pointer">ShopEase</span>
+        <span className="text-sm cursor-pointer hover:underline">Shop</span>
+        <span className="text-sm cursor-pointer hover:underline">Best Selllets</span>
+        <span className="text-sm cursor-pointer hover:underline">Deals</span>
+      </div>
+      <div className="hidden items-center justify-center gap-5 md:flex ">
+        <div className="flex items-center justify-center gap-2 cursor-pointer">
+            <img src={cartIcon} alt="Cart"/>
+            <span className="text-sm">Cart</span>
+        </div>
+        <span className="text-sm cursor-pointer hover:underline">Help</span>
+        <span className="text-sm cursor-pointer hover:underline">Login</span>
+        <span className="text-sm bg-white text-black py-2 px-3 rounded-full font-medium cursor-pointer">Sign up</span>
+
+      </div>
+    </div>
+  )
+};
 
 export default Navbar
